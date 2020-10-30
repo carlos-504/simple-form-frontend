b15712e4e275c6433dddb0795f46d692dc81e88f
Carlos Henrique <carllosshennriique@gmail.com>
2020-10-29 22:22:22 -0300

    Adição da pagina de edição de ususários

--- src/Routes.js
+++ src/Routes.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import Main from "./pages/Main";
 import Listar from "./pages/Listar";
+import Editar from "./pages/Editar"
 
 function Routes() {
   return (
@@ -10,6 +11,7 @@ function Routes() {
       <Switch>
         <Route path="/" exact component={Main} />
         <Route path="/listar" exact component={Listar} />
+        <Route path="/editar" exact component={Editar} />
       </Switch>
     </BrowserRouter>
   );
---
+++ src/pages/Editar/index.js
@@ -0,0 +1,25 @@
+import { Container, Button } from "@material-ui/core";
+import { Link } from 'react-router-dom'
+import React from "react";
+import Navbar from "../../components/Navbar";
+import Form from "../../components/Form";
+import "./style.css";
+
+function editarForm() {
+  return (
+    <>
+      <Navbar />
+      <Container component="div" maxWidth="md">
+        <h1>Editar Usuário</h1>
+        <Form />
+        <div className="button_voltar">
+            <Link to="/listar">
+                <Button variant="contained" color="secondary">Voltar</Button>
+            </Link>
+        </div>
+      </Container>
+    </>
+  );
+}
+
+export default editarForm;
