cc5632e0877b44bdd6471bc78e4613e9c9a69615
Carlos Henrique <carllosshennriique@gmail.com>
2020-11-03 15:58:26 -0300

    Implementação da exclusão de usuários

--- src/components/DataTable/index.js
+++ src/components/DataTable/index.js
@@ -7,8 +7,10 @@ import {
   TableHead,
   TableRow,
   Paper,
+  Button
 } from "@material-ui/core";
 import { withStyles, makeStyles } from "@material-ui/core/styles";
+import Alert from '@material-ui/lab/Alert';
 import api from "../../api";
 
 const StyledTableCell = withStyles((theme) => ({
@@ -27,8 +29,17 @@ const useStyles = makeStyles({
   },
 });
 
+const useAlerts = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    '& > * + *': {
+      marginTop: theme.spacing(2),
+    },
+  },
+}));
+
 function DataTable() {
-  const classes = useStyles();
+  const classes = [useStyles(), useAlerts()];
   const [usuarios, setUsuario] = useState([]);
 
   useEffect(() => {
@@ -38,39 +49,54 @@ function DataTable() {
         })
         .catch(erro => console.log(erro))
   }, [])
+
+  async function handleExcluir(id){
+    if(window.confirm("Deseja realmente excluir esse usuário")){
+      const result = await api.delete('/usuarios/' + id)
+      if(result.status === 200) {
+        window.location.href = '/listar'
+      } else {
+        alert('Ocorreu um erro')
+      }
+    }
+  }
   
   return (
-    <TableContainer component={Paper} >
-      <Table className={classes.table} aria-label="simple table" >
-        <TableHead>
-          <TableRow>
-            <StyledTableCell align="left" variant="head">ID</StyledTableCell>
-            <StyledTableCell align="left" variant="head">Nome</StyledTableCell>
-            <StyledTableCell align="left" variant="head">Sobrenome</StyledTableCell>
-            <StyledTableCell align="left" variant="head">Email</StyledTableCell>
-            <StyledTableCell align="left" variant="head">Telefone</StyledTableCell>
-            <StyledTableCell align="left" variant="head">Celular</StyledTableCell>
-            <StyledTableCell align="left" variant="head">Cidade</StyledTableCell>
-            <StyledTableCell align="center" variant="head" colSpan="2" >Ações</StyledTableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-            {usuarios.map((usuario) => (
-                <TableRow key={usuario.id} hover={true}>
-                  <TableCell align="left" variant="body">{usuario.id}</TableCell>
-                  <TableCell align="left" variant="body">{usuario.nome}</TableCell>
-                  <TableCell align="left" variant="body">{usuario.sobrenome}</TableCell>
-                  <TableCell align="left" variant="body">{usuario.email}</TableCell>
-                  <TableCell align="left" variant="body">{usuario.telefone}</TableCell>
-                  <TableCell align="left" variant="body">{usuario.celular}</TableCell>
-                  <TableCell align="left" variant="body">{usuario.cidade}</TableCell>
-                  <TableCell align="left" variant="body">Editar</TableCell>
-                  <TableCell align="left" variant="body">Excluir</TableCell>
-                </TableRow>    
-            ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <>
+      <TableContainer component={Paper} >
+        <Table className={classes.table} aria-label="simple table" >
+          <TableHead>
+            <TableRow>
+              <StyledTableCell align="left" variant="head">ID</StyledTableCell>
+              <StyledTableCell align="left" variant="head">Nome</StyledTableCell>
+              <StyledTableCell align="left" variant="head">Sobrenome</StyledTableCell>
+              <StyledTableCell align="left" variant="head">Email</StyledTableCell>
+              <StyledTableCell align="left" variant="head">Telefone</StyledTableCell>
+              <StyledTableCell align="left" variant="head">Celular</StyledTableCell>
+              <StyledTableCell align="left" variant="head">Cidade</StyledTableCell>
+              <StyledTableCell align="center" variant="head" colSpan="2" >Ações</StyledTableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+              {usuarios.map((usuario) => (
+                  <TableRow key={usuario.id} hover={true}>
+                    <TableCell align="left" variant="body">{usuario.id}</TableCell>
+                    <TableCell align="left" variant="body">{usuario.nome}</TableCell>
+                    <TableCell align="left" variant="body">{usuario.sobrenome}</TableCell>
+                    <TableCell align="left" variant="body">{usuario.email}</TableCell>
+                    <TableCell align="left" variant="body">{usuario.telefone}</TableCell>
+                    <TableCell align="left" variant="body">{usuario.celular}</TableCell>
+                    <TableCell align="left" variant="body">{usuario.cidade}</TableCell>
+                    <TableCell align="left" variant="body" >Editar</TableCell>
+                    <TableCell align="left" variant="body">
+                      <Button onClick={() => handleExcluir(usuario.id)}>Excluir</Button>
+                    </TableCell>
+                  </TableRow>    
+              ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
   );
 }
 
